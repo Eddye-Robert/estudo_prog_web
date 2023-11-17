f942254914f0a574b573d7aa399e093e3ad1b59c
iclimia112 <eddye.robert06@aluno.ifce.edu.br>
2023-11-17 15:44:33 -0300

    atvprat03

---
+++ ATIVIDADES/atividadeprat03/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const port = 3000
+
+//Configure template handlebars
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+//parser para leitura de body
+app.use(
+    express.urlencoded({
+        extended:true
+    })
+)
+app.use(express.json())
+
+//adicionando css
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => {
+    console.log('Server Started')
+})
